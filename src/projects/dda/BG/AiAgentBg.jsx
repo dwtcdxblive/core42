a6625385a578f372a4dba23c06782be435447747
MarwaAlnaqar <marwa.alnaqar@dwtc.com>
2025-10-12 20:45:44 +0400

    aiagent

--- src/projects/dda/BG/AiAgentBg.jsx
+++ src/projects/dda/BG/AiAgentBg.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import AI1 from "../../../assets/bgDDA/AI1.svg";
-import AI2 from "../../../assets/bgDDA/AI2.svg";
-import AI3 from "../../../assets/bgDDA/AI3.svg";
+import AI1 from "../../../assets/bgDDA/AI1.png";
+import AI2 from "../../../assets/bgDDA/AI2.png";
+
 import { RedirectBase } from "../RedirectBase";
 const AiAgentBg = () => {
   const imageList = [
     { src: AI1, link: "" },
     { src: AI2, link: "" },
-    { src: AI3, link: "" },
+
   ];
 
   return (
-    <main className="min-vh-100 d-flex align-items-center justify-content-center bg-light">
+    <main className="min-vh-100 d-flex align-items-center justify-content-center   ">
       <div className="container">
         <div className="row justify-content-center g-4">
           {imageList.map((item, index) => (
             <div
               key={index}
-              className="col-12 col-md-4 d-flex justify-content-center"
+              className="col-12 col-md-6 d-flex justify-content-center"
             >
               <img
                 src={item.src}
